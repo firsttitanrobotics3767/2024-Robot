d71c4ccb6128fc45bf6f07ec37ad42371ebbea23
CH-FRC3767 <hetherincol90@tcapsstudent.net>
2024-02-08 19:57:27 -0500

    Added Shooter.java
    Added Shooter.java and built out methods with started Javadoc comments.

---
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -0,0 +1,32 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+/**
+ * Shooter class
+ */
+public class Shooter extends SubsystemBase{
+    /**
+     * Set the rotational position of the shooter
+     * @param position double of the anglular position
+     */
+    public void setRotationalPos(double position) {
+        //Code
+    }
+
+    /**
+     * Set the velocity of the shooter
+     * @param speed double of the velocity
+     */
+    public void setShootSpeed(double speed) {
+        //Code
+    }
+
+    /**
+     * Set the velocity of the feeder
+     * @param speed double of the velocity
+     */
+    public void setFeederSpeed(double speed) {
+        //Code
+    }
+}
