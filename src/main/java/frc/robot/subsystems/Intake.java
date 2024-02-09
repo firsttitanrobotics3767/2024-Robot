618271faac75f6f3f56442ddebfc102e6aaf1753
CH-FRC3767 <hetherincol90@tcapsstudent.net>
2024-02-08 20:03:07 -0500

    Create Intake.java

---
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -0,0 +1,24 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+/**
+ * Intake class
+ */
+public class Intake extends SubsystemBase{
+    /**
+     * Set the velocity of the intake
+     * @param speed double of the velocity
+     */
+    public void setIntakeSpeed(double speed) {
+        //Code
+    }
+
+    /**
+     * Set the position of the intake
+     * @param position double of the intake position
+     */
+    public void setPos(double position) {
+        //Code
+    }
+}
